5ad3b281496bce07e3dc3c232cdf73923ef6a6ff
GitLabOdix <azaheni@smartgraphe.com>
2019-12-20 20:30:40 +0100

    first commit

---
+++ src/main/java/com/odix/fr/FichiersMsApplication.java
@@ -0,0 +1,13 @@
+package com.odix.fr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FichiersMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FichiersMsApplication.class, args);
+	}
+
+}
