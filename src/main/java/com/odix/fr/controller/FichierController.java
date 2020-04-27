a60c14a1e844fcae9303cf30c547b54fa34de281
odix <odix@odix-laptop>
2020-04-27 06:55:58 +0100

    Suppression CrossOrigin

--- src/main/java/com/odix/fr/controller/FichierController.java
+++ src/main/java/com/odix/fr/controller/FichierController.java
@@ -3,7 +3,6 @@ package com.odix.fr.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.odix.fr.model.Fichier;
 import com.odix.fr.service.FichierService;
 
-@CrossOrigin
+
 @RestController
 @RequestMapping("/api/fichier")
 public class FichierController {
